3ffdd24e7e04e461281cfa0b075671fea189958f
Yasykur Rafii <34973363+yasykurrafii@users.noreply.github.com>
2022-01-06 15:27:27 +0700

    up_sql

---
+++ db_sql.sql
@@ -0,0 +1,33 @@
+-- buat bikin user raspberry nya 
+show grants for 'rasp1'@'192.168.25.3';
+create user 'rasp2'@'192.168.25.3' identified by 'rasp2';
+grant all privileges on *.*  to 'rasp2'@'192.168.25.3';
+flush privileges;
+
+
+CREATE TABLE dht(
+	id INT NOT NULL AUTO_INCREMENT,
+    ip VARCHAR(12) NOT NULL,
+    suhu INT NOT NULL,
+    humidity INT NOT NULL,
+    tanggal DATETIME DEFAULT current_timestamp,
+    primary key (id)
+);
+
+CREATE TABLE rly(
+	id INT NOT NULL AUTO_INCREMENT,
+    ip VARCHAR(12) NOT NULL,
+    gpio INT NOT NULL,
+    kondisi INT NOT NULL,
+    tanggal DATETIME DEFAULT current_timestamp,
+    primary key (id)
+);
+
+CREATE TABLE skl(
+	id INT NOT NULL AUTO_INCREMENT,
+    ip VARCHAR(12) NOT NULL,
+    gpio INT NOT NULL,
+    kondisi INT NOT NULL,
+    tanggal DATETIME DEFAULT current_timestamp,
+    primary key (id)
+);
